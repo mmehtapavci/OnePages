172d2cf8844d978d7e9e845ceabbcbb588d9a8f1
mmehtapavci <mmmehtapavci@gmail.com>
2023-10-30 23:05:19 +0300

    Regulations-3

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ $(document).ready(function () {
     $("#accordion").accordion();
 })
 
+$(document).ready(function () {
+    $("#dialogButton").click(function () {
+        $("#dialog").dialog();
+    });
+});
