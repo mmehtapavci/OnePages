adf8d127c4ef301bf70a21b4a3495f2a5ee4b0f4
mmehtapavci <mmmehtapavci@gmail.com>
2023-10-31 21:26:53 +0300

    Finish

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ $(document).ready(function () {
 })
 
 $(function () {
+    $("#dialog").dialog({
+        autoOpen: false
+    });
     $("#dialogButton").click(function () {
-        $("#dialog").dialog();
+        $("#dialog").dialog("open");
+    });
+});
+
+// Seach
+
+// $(function () {
+//     var autoComplate = [
+//         "Html5",
+//         "Asp",
+//         "C",
+//         "C++",
+//         "Css3",
+//         "JavaScript",
+//         "Jquery"
+//     ];
+//     $("#tags").autoComplate({
+//         source: autoComplate
+//     });
+// });
+$(function () {
+    var autoComplete = [
+        "Html5",
+        "Asp",
+        "C",
+        "C++",
+        "Css3",
+        "JavaScript",
+        "Jquery"
+    ];
+
+    $("#tags").autocomplete({
+        source: autoComplete
     });
 });
